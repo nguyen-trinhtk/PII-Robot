f0cd3c94f327c852a9f026e00972a27aaec1a7f0
nguyen-trinhtk <khanhnguyentrinhthi@gmail.com>
2023-07-05 15:57:30 +0700

    July 5, 15:55
    Modified bottle.py:
    - Added markup methods for creating a formula
    - Added distance methods for finding distance
    - Distance-pixel relation not yet found (shown formula serves as an example)

--- Bottle Detection/bottle.py
+++ Bottle Detection/bottle.py
@@ -1,6 +1,7 @@
 from imageai.Detection import ObjectDetection
 import os
 import cv2
+import math 
 
 execution_path = os.getcwd()
 
@@ -10,6 +11,44 @@ if not cam.isOpened():
     exit()
  
 cnt = 0
+
+#divide frame to form a relation btw distance irl and in img
+def markupY(frame, section):
+    x = frame.shape[1]//2
+    y = frame.shape[0]
+    xleft = x - 5
+    xright = x + 5
+    cv2.line(frame, (x,0), (x,y), (0,0,255), 2)
+    for i in range(section-1):
+         yTemp = int((i+1)*y/section)
+         cv2.line(frame, (xleft,yTemp), (xright, yTemp), (0,0,255), 2)
+
+def markupX(frame, section):
+    x = frame.shape[1]
+    y = frame.shape[0]
+    yUp = y - 7
+    cv2.line(frame, (0,y), (x,y), (0,0,255), 2)
+    for i in range(section-1):
+         xTemp = int((i+1)*x/section)
+         cv2.line(frame, (xTemp,yUp), (xTemp, y), (0,0,255), 2)
+
+#calculate distance and angle based on found formula
+def distance(frame,object):
+    endY = frame.shape[0]
+    midX = int(frame.shape[1]//2)
+    difX = midX - (object["box_points"][0] + object["box_points"][2])//2
+    difY = endY - (object["box_points"][1] + object["box_points"][3])//2
+    #formula
+    distanceY = 2*(int(difY/endY)*4+1)
+    distanceX = 2*(int(difX/midX)*4+1)
+    if difX == 0:
+         absAngle = 90
+    else: absAngle = math.atan(distanceY/distanceX)*180 / math.pi
+    hypoDistance = (distanceY**2 + distanceX**2)**0.5
+    return (absAngle, hypoDistance)
+
+    
+
 while True:
     ret, frame = cam.read()
     cnt += 1
@@ -19,7 +58,7 @@ while True:
 
         detector = ObjectDetection()
         detector.setModelTypeAsYOLOv3()
-        detector.setModelPath(os.path.join(execution_path , "yolov3.pt"))
+        detector.setModelPath(os.path.join(execution_path , "Bottle Detection\yolov3.pt"))
         detector.loadModel()
         detections = detector.detectObjectsFromImage(input_image=frame, 
                                                     minimum_percentage_probability=30,
@@ -28,19 +67,19 @@ while True:
 
         for eachObject in detections:
             if eachObject['name']=='bottle':
+                print ('Object {} cm away at an angle of {} degrees'.format(distance(frame,eachObject)[1], distance(frame,eachObject)[0]))
                 print('Bottle detected with probability: {}%'.format(eachObject["percentage_probability"]))
                 # print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
                 print("--------------------------------")
                 
-                # start_point = [eachObject["box_points"][0], eachObject["box_points"][1]]
-                # end_point = [eachObject["box_points"][2], eachObject["box_points"][3]]
+                start_point = [eachObject["box_points"][0], eachObject["box_points"][1]]
+                end_point = [eachObject["box_points"][2], eachObject["box_points"][3]]
                 
-                # # Blue color in BGR
-                # color = (255, 0, 0)
-                # # Line thickness of 2 px
-                # thickness = 2
-
-                # frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
+                # Blue color in BGR
+                color = (255, 0, 0)
+                # Line thickness of 2 px
+                thickness = 2
+                frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
 
     if not ret:
             break
