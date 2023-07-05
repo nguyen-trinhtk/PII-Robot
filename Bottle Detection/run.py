9ab6061bb57052edd8adfe635e105e2ece108898
nguyen-trinhtk <khanhnguyentrinhthi@gmail.com>
2023-07-05 08:20:09 +0700

    July 5, 8:20

--- Bottle Detection/run.py
+++ Bottle Detection/run.py
@@ -22,27 +22,28 @@ while True:
         detector.setModelPath(os.path.join(execution_path , "yolov3.pt"))
         detector.loadModel()
         detections = detector.detectObjectsFromImage(input_image=frame, 
-                                                    output_image_path=os.path.join(execution_path , "image2new.jpg"), 
                                                     minimum_percentage_probability=30,
                                                     display_percentage_probability = True,
                                                     display_object_name = True)
 
         for eachObject in detections:
-            print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-            print("--------------------------------")
-            
-            start_point = [eachObject["box_points"][0], eachObject["box_points"][1]]
-            end_point = [eachObject["box_points"][2], eachObject["box_points"][3]]
-            
-            # Blue color in BGR
-            color = (255, 0, 0)
-            # Line thickness of 2 px
-            thickness = 2
+            if eachObject['name']=='bottle':
+                print('Bottle detected with probability: {}%'.format(eachObject["percentage_probability"]))
+                # print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+                print("--------------------------------")
+                
+                # start_point = [eachObject["box_points"][0], eachObject["box_points"][1]]
+                # end_point = [eachObject["box_points"][2], eachObject["box_points"][3]]
+                
+                # # Blue color in BGR
+                # color = (255, 0, 0)
+                # # Line thickness of 2 px
+                # thickness = 2
 
-            frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
+                # frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
 
     if not ret:
-        break
+            break
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
--- Bottle Detection/training.py
+++
@@ -1,7 +0,0 @@
-from imageai.Detection.Custom import DetectionModelTrainer
-
-trainer = DetectionModelTrainer()
-trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="bottle-dataset")
-trainer.setTrainConfig(object_names_array=["Plastic-Bottle"], batch_size=4, num_experiments=200, train_from_pretrained_model="yolov3.pt")
-trainer.trainModel()
