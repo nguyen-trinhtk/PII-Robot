3633403a958fa2e67b09aa788d3e76837214568d
nguyen-trinhtk <khanhnguyentrinhthi@gmail.com>
2023-07-05 11:34:11 +0700

    July 5, 11:34

--- RPLidar/lidar.py
+++ RPLidar/lidar.py
@@ -7,11 +7,12 @@ print(info)
 health = lidar.get_health()
 print(health)
 
-for i, scan in enumerate(lidar.iter_scans()):
-    print('%d: Got %d measurments' % (i, len(scan)))
-    if i > 10:
+for i, measurement in enumerate(lidar.iter_measures()):
+    print('Angle: {} degrees'.format(int(measurement[2])))
+    print('Distance {} mm'.format(measurement[3]))
+    if i > 100:
         break
-
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
+while True: 
+    lidar.stop()
+    lidar.stop_motor()
+# lidar.disconnect()
