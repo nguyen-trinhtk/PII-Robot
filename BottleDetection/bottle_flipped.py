e37e0065290e35295b873dad627b8676b843a6f8
nguyen-trinhtk <khanhnguyentrinhthi@gmail.com>
2023-07-19 08:12:18 +0700

    July 19, 8:12
    Rename files

--- BottleDetection/bottle_flipped.py
+++ BottleDetection/bottle_flipped.py
@@ -1,35 +1,36 @@
 from imageai.Detection import ObjectDetection
 import os
 import cv2
-import math 
-
-
+import math
+import time
+# Get the current working directory
 execution_path = os.getcwd()
-
+# Initialize the camera
 cam = cv2.VideoCapture(0)
-
+# Check if the camera is opened
 if not cam.isOpened():
     print('cam is not opening')
     exit()
- 
+#Initializing variables
 cnt = -1
-
-#calculate distance and angle based on found formula
+lastTime = 0
+thisTime = 0
 def info(frame,object):
-    endY = frame.shape[0]
-    midX = int(frame.shape[1]//2)
-    difX = midX - (object["box_points"][0] + object["box_points"][2])//2
-    difY = endY - (object["box_points"][1] + object["box_points"][3])//2
-    dY = endY - max(object["box_points"][1],object["box_points"][3])
-    #formula
-    distanceY = (142.902*(math.e**(2.512*(dY/endY)))-132.985+400)
-    if difY == 0:
-         absAngle = 0
-    else: absAngle = math.atan(difX/difY)
-    distance = distanceY/math.cos(absAngle)
+    """Calculate the angle and distance of the object from the frame."""
+    endX = frame.shape[1]
+    midY = int(frame.shape[0]//2)
+    difY = midY - (object["box_points"][1] + object["box_points"][3])//2
+    difX = endX - (object["box_points"][0] + object["box_points"][2])//2
+    dX = endX - max(object["box_points"][0],object["box_points"][2])
+    distanceX = (142.902*(math.e**(2.512*(dX/endX)))-132.985+400)
+    if difX == 0:
+        absAngle = 0
+    else: 
+        absAngle = math.atan(difY/difX)
+    distance = distanceX/math.cos(absAngle)
     return int(math.degrees(absAngle)), int(distance)
-
 def detect(frame):
+    """Detect the object in the frame."""
     detections =  detector.detectObjectsFromImage(input_image=frame,
                                                     minimum_percentage_probability=30,
                                                     display_percentage_probability = True,
@@ -37,20 +38,21 @@ def detect(frame):
     for eachObject in detections:
         if eachObject['name']=='bottle':
             return eachObject
-
 def center(object):
+    """Adjust the camera to center the object."""
     adjust = True
     frc = 0
     while (adjust == True):
         frc += 1
         if (frc%30 == 0):
             ret, frame = cam.read()
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
             object = detect(frame)
             if (object):
                 cv2.imshow('frame', frame)
                 angle = info(frame,object)[0]
                 print('Current angle: ' + str(angle))
-                if (abs(angle) <= 5):
+                if (abs(angle) <= 10):
                     print('centered')
                     adjust = False
                 elif (angle < -45):
@@ -65,29 +67,35 @@ def center(object):
                 print('Object out of frame')
     print ('Bottle {} mm away, centered'.format(info(frame,object)[1], info(frame,object)[0]))
     print("--------------------------------")
-
+# Initialize the object detector
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(execution_path, "BottleDetection\models\yolov3.pt"))
 detector.loadModel()
-
 while True:
+    # lastTime = thisTime
     cnt += 1
     ret, frame = cam.read()
-    if (cnt%30==0):
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    # thisTime = time.time()
+    if (cnt%15==0):
         object = detect(frame)
         if object:
             print('Bottle detected with probability: {}%'.format(object["percentage_probability"]))
             print('Bottle {} mm away at an angle of {} degrees'.format(info(frame,object)[1], info(frame,object)[0]))
-            center(object)
+            # center(object)
+            start_point = [object["box_points"][0], object["box_points"][1]]
+            end_point = [object["box_points"][2], object["box_points"][3]]
+            color = (255, 0, 0)  # Blue color in BGR
+            thickness = 2  # Line thickness of 2 px
+            frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
         else:
             print('No bottle found')
+        # print(str(1/(thisTime-lastTime)))
     if not ret:
-            break
-
-    cv2.imshow('frame', frame)
+        break
+    cv2.imshow('frame', cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE))
     if cv2.waitKey(1)==ord('q'):
         break
-
 cam.release()
 cv2.destroyAllWindows()
