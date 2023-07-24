ce7ae869632f85462ec62ca75926f206ff60cc11
nguyen-trinhtk <khanhnguyentrinhthi@gmail.com>
2023-07-24 09:58:41 +0700

    July 24, 9:58
    Added code for calculating FPS and serial communication

--- BottleDetection/calFPS.py
+++ BottleDetection/calFPS.py
@@ -2,16 +2,20 @@ from imageai.Detection import ObjectDetection
 import os
 import cv2
 import math
-# Get the current working directory
+import time
+ # Get the current working directory
 execution_path = os.getcwd()
-# Initialize the camera
+ # Initialize the camera
 cam = cv2.VideoCapture(0)
-# Check if the camera is opened
+ # Check if the camera is opened
 if not cam.isOpened():
     print('cam is not opening')
     exit()
-#Initializing variables
-cnt = -1
+ # Initialize the object detector
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(execution_path, "BottleDetection\models\yolov3.pt"))
+detector.loadModel()
 def info(frame,object):
     """Calculate the angle and distance of the object from the frame."""
     endX = frame.shape[1]
@@ -20,31 +24,26 @@ def info(frame,object):
     difX = endX - (object["box_points"][0] + object["box_points"][2])//2
     dX = endX - max(object["box_points"][0],object["box_points"][2])
     distanceX = (142.902*(math.e**(2.512*(dX/endX)))-132.985+400)
-    if difX == 0:
-        absAngle = 0
-    else: 
-        absAngle = math.atan(difY/difX)
+    absAngle = 0 if difX == 0 else math.atan(difY/difX)
     distance = distanceX/math.cos(absAngle)
     return int(math.degrees(absAngle)), int(distance)
+
 def detect(frame):
     """Detect the object in the frame."""
     detections =  detector.detectObjectsFromImage(input_image=frame,
                                                     minimum_percentage_probability=80,
                                                     display_percentage_probability = True,
                                                     display_object_name = True)
-    for eachObject in detections:
-        if eachObject['name']=='bottle':
-            return eachObject
-        
-def center():
+    return next((eachObject for eachObject in detections if eachObject['name']=='bottle'), None)
+
+def center(frame):
     """Adjust the camera to center the object."""
-    adjust = True
     frc = 0
-    while (adjust == True):
+    while True:
         frc += 1
+        ret, frame = cam.read()
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
         if (frc%30 == 0):
-            ret, frame = cam.read()
-            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
             object = detect(frame)
             if (object):
                 cv2.imshow('frame', frame)
@@ -52,42 +51,30 @@ def center():
                 print('Current angle: ' + str(angle))
                 if (abs(angle) <= 10):
                     print('centered')
-                    adjust = False
-                elif (angle < -45):
-                    print('far right')
-                elif (angle < 0):
-                    print('near right')
-                elif (angle < 45):
-                    print('near left')
-                else:
-                    print('far left')
+                    return
+                print('Current angle: {} '.format(str(angle)) + ('far right' if angle < -45 else 'near right' if angle < 0 else 'near left' if angle < 45 else 'far left'))
             else:
                 print('Object out of frame')
-    print ('Bottle {} mm away, centered'.format(info(frame,object)[1], info(frame,object)[0]))
-    print("--------------------------------")
-# Initialize the object detector
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path, "BottleDetection\models\yolov3.pt"))
-detector.loadModel()
+        if not ret:
+            break
+        cv2.imshow('frame', cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE))
+    
 
+cnt = -1
 while True:
+    startTime = time.time()
     cnt += 1
     ret, frame = cam.read()
     frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-    if (cnt%60==0):
+    if (cnt%30==0):
         object = detect(frame)
+        print("Detecting FPS rate: ", 1.0 / (time.time() - startTime))
         if object:
-            print('Bottle detected with probability: {}%'.format(object["percentage_probability"]))
             print('Bottle {} mm away at an angle of {} degrees'.format(info(frame,object)[1], info(frame,object)[0]))
-            center()
-            start_point = [object["box_points"][0], object["box_points"][1]]
-            end_point = [object["box_points"][2], object["box_points"][3]]
-            color = (255, 0, 0)  # Blue color in BGR
-            thickness = 2  # Line thickness of 2 px
-            frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
         else:
             print('No bottle found')
+    else:
+        print("FPS: ", 1.0 / (time.time() - startTime))
     if not ret:
         break
     cv2.imshow('frame', cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE))
